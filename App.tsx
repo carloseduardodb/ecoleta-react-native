04f6fc3a8eb75abbf8638fb27c49c05441278817
Carlos <carloscangere@hotmail.com>
2021-03-14 01:35:53 -0300

    next update api php relations

--- App.tsx
+++ App.tsx
@@ -8,47 +8,32 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+ import React from 'react';
+ import { StatusBar } from 'react-native';
+ import { AppLoading } from 'expo';
+ import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+ import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+ 
+ import Routes from './src/routes';
+ 
+ const App = () => {
+   const [fontsLoaded] = useFonts({
+     Roboto_400Regular,
+     Roboto_500Medium,
+     Ubuntu_700Bold
+   });
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+   if(!fontsLoaded){
+     return <AppLoading/>
+   }
 
-declare const global: {HermesInternal: null | {}};
-
-const App = () => {
-  return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello World</Text>
-      </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  text:{
-    backgroundColor: "#7159c1",
-    color: "white"
-  },
-
-  container:{
-    flex: 1,
-    backgroundColor: "#7159c1",
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-
-});
-
-export default App;
+   return (
+     <>
+       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+       <Routes />
+     </>
+   );
+ };
+ 
+ export default App;
+ 
